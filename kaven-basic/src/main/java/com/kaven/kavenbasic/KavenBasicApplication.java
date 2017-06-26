e5d0a5d21e7793296389d70b39becf82cb5da048
wang_wei <wang_wei05@uisftech.com>
2017-06-26 11:22:52 +0800

    1 快速构建spring boot 项目
    jdk1.8 spring boot 1.4.6

---
+++ kaven-basic/src/main/java/com/kaven/kavenbasic/KavenBasicApplication.java
@@ -0,0 +1,12 @@
+package com.kaven.kavenbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KavenBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KavenBasicApplication.class, args);
+	}
+}
